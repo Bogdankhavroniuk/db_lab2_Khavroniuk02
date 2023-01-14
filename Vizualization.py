cd5b4f6c1598c848501142250beced09b1674fc9
Bogdankhavroniuk <60515937+Bogdankhavroniuk@users.noreply.github.com>
2023-01-14 23:38:50 +0200

    Update Vizualization.py

--- Vizualization.py
+++ Vizualization.py
@@ -56,10 +56,10 @@ class Get_visualisation():
         p_w = self.password
 
         conect_parametrs = "dbname="+  d_n +" user="+  u_n +" password=" +p_w
-        print(conect_parametrs)
+
         dat = conect_parametrs
 
-        conn = ps.connect("dbname=Khavroniuk_lr_1 user=user1 password=123321 ")
+        conn = ps.connect("dbname=lr2 user=user1 password=123321 ")
 
 
 
@@ -87,45 +87,25 @@ class Get_visualisation():
 
 
 
-    def get_column_gistogram(self , query ):
-            column1 =  self.get_column(query)
-            try:
+    def get_column_gistogram(self , query , query1 ):
+              column1 =  self.get_column(query)
+              column2 = self.get_column(query1)
               column  = get_column_data(column1 )
-            
-              p = float(1.322)
 
-              diapazon = max(column) - min(column)
+              values = get_string_data(column2)
+              fig = plt.figure(figsize=(10, 10))
+              plt.bar( values, column, color='maroon' , width = 0.4)
 
-              n = m.ceil(1 + (p * m.log10(50)))
-
-              h = diapazon / n
-
-              list_of_intervals = []
-              patches = []
-              x = 1
-              i = 1
-
-              patches.append(x)
-
-              while i <= n:
-                  list_of_intervals.append(x)
-                  i += 1
-                  x += h
-                  list_of_intervals.append(x)
-                  patches.append(x)
-            
+              plt.xlabel("name of scorer")
+              plt.ylabel("sccorer points")
+              plt.title("The geatest scorer")
 
 
 
-
-              plt.hist(column, bins=list_of_intervals, edgecolor='black')
-              plt.xticks(patches)
-              #plt.savefig('Gistogram.png')
               plt.show()
               plt.close()
-            except:
-                print("Wrong data input")
-            return column
+
+              return column1
 
 
 
@@ -151,6 +131,7 @@ class Get_visualisation():
                    ax.pie(data_values, labels=data_names)
                    ax.axis("equal")
                    #plt.savefig('circle_diagram.png')
+                   plt.title("The geatest scorer")
                    plt.show()
                    plt.close()
             except:
@@ -173,18 +154,16 @@ class Get_visualisation():
             column2 = get_column_data(column2)
             if len(column1) != len(column2):
                 raise Exception("Dimention_exeption")
-         
+
             if isinstance(column2[0], int ) == False or isinstance(column1[0], int) == False:
                     raise Exception("Data_typpe_exeption")
             else:
-               column1  =  get_column_data(column1)
-               column2  =  get_column_data(column2)
+
                fig, ax = plt.subplots()
 
                ax.plot(column1 , column2 , linewidth=2.0)
-               plt.xlabel(column_of_values1 , column_of_values2 )
-               plt.ylabel('values')
-               ax.set(xlim=(0, 8), xticks=np.arange(1, 8),ylim=(0, 8), yticks=np.arange(1, 8))
+
+               plt.title("Dependensy bbetwen field goals and total points")
                #plt.savefig('dependency_diagram.png')
                plt.show()
                plt.close()
@@ -192,7 +171,7 @@ class Get_visualisation():
             print("cant build graph from this query")
             
 
-        return  data_values  ,  data_names
+        return  column1  ,  column2
 
 
 
