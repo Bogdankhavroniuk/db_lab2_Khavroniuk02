15cd73fc3b3498853f3a4a5e3ae8ccf7524e18e8
Bogdankhavroniuk <60515937+Bogdankhavroniuk@users.noreply.github.com>
2023-01-14 23:42:31 +0200

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,6 @@
+SELECT   total_points FROM statistic
+SELECT trim(player_name) FROM player
+SELECT total_points  FROM statistic
+SELECT trim(player_name) FROM player
+SELECT field_goals  FROM statistic
+SELECT total_points  FROM statistic
